e387d2cebcafb4f390ab74a1447aac1322eb7986
WoBen07 <benedict.wohlgemuth@uni-a.de>
2025-05-08 20:07:12 +0200

    setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,40 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_SHT31.h>
+#include <BH1750.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define SDA_PIN 18
+#define SCL_PIN 35
+
+Adafruit_SHT31 sht30 = Adafruit_SHT31();
+BH1750 lightMeter;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  delay(1000);
+
+  Wire.begin(SDA_PIN, SCL_PIN);
+
+  if (!sht30.begin(0x44)) {
+    Serial.println("Couldn't find SHT30");
+    while (1) delay(1);
+  }
+
+  if (!lightMeter.begin(BH1750::CONTINUOUS_HIGH_RES_MODE)) {
+    Serial.println("Couldn't find BH1750");
+    while (1) delay(1);
+  }
+
+  Serial.println("Sensors initialized.");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  float temperature = sht30.readTemperature();
+  float humidity = sht30.readHumidity();
+  float lux = lightMeter.readLightLevel();
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  Serial.printf("Temp: %.2f °C, Humidity: %.2f %%, Light: %.2f lx\n",
+                temperature, humidity, lux);
+
+  delay(5000);
 }
